e440a89c83a3f39cca171b136a8a4c3918aab961
DennySPB <dennyspb@gmail.com>
2018-03-14 17:05:36 +0300

    eas

--- msm8996.mk
+++ msm8996.mk
@@ -42,6 +42,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+PRODUCT_GENERIC_PROPERTIES += \		     
+    use.dts_eagle=true \
+    hpx_send_params=1
 
 # HWUI overrides
 # $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
